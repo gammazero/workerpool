f434e3633ce92a30a68e52b3b164078772521e7c
Andrew Gillis <gillis.andrewj@gmail.com>
2017-10-04 13:58:07 -0400

    pacer and workerpool are independent, so put in separate packages

--- pacer/pacer.go
+++ pacer/pacer.go
@@ -1,4 +1,15 @@
-package workerpool
+/*
+Package pacer provides a utility to limit the rate at which concurrent
+goroutines begin execution.  This addresses situations where running the
+concurrent goroutines is OK, as long as their execution does not start at the
+same time.
+
+The pacer package is independent of the workerpool package.  Paced functions
+can be submitted to a workerpool or can be run as goroutines, and execution
+will be paced in both cases.
+
+*/
+package pacer
 
 import "time"
 
--- workerpool_test.go
+++ workerpool_test.go
@@ -171,60 +171,6 @@ func TestSubmitWait(t *testing.T) {
 	}
 }
 
-func TestPacedWorkers(t *testing.T) {
-	t.Parallel()
-
-	delay1 := 100 * time.Millisecond
-	delay2 := 300 * time.Millisecond
-	wp := New(5)
-	defer wp.Stop()
-
-	pacer := NewPacer(delay1, true)
-	defer pacer.Stop()
-
-	slowPacer := NewPacer(delay2, false)
-	defer slowPacer.Stop()
-
-	tasksDone := new(sync.WaitGroup)
-	tasksDone.Add(20)
-	start := time.Now()
-
-	pacedTask := pacer.Pace(func() {
-		//fmt.Println("Task")
-		tasksDone.Done()
-	})
-
-	slowPacedTask := slowPacer.Pace(func() {
-		//fmt.Println("SlowTask")
-		tasksDone.Done()
-	})
-
-	// Cause worker to be created, and available for reuse before next task.
-	for i := 0; i < 10; i++ {
-		wp.Submit(pacedTask)
-		wp.Submit(slowPacedTask)
-	}
-
-	time.Sleep(500 * time.Millisecond)
-	pacer.Pause()
-	time.Sleep(time.Second)
-	if !pacer.IsPaused() {
-		t.Fatal("should be paused")
-	}
-	pacer.Resume()
-	if pacer.IsPaused() {
-		t.Fatal("should not be paused")
-	}
-
-	tasksDone.Wait()
-	elapsed := time.Now().Sub(start)
-	// 9 times delay2 since no wait for first task, and pacer and slowPacer run
-	// currently so only limiter is slowPacer.
-	if elapsed < 9*delay2 {
-		t.Fatal("Did not pace tasks correctly - finished too soon:", elapsed)
-	}
-}
-
 func anyReady(w *WorkerPool) bool {
 	select {
 	case wkCh := <-w.readyWorkers:
