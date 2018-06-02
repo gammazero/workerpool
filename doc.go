7172a7c5e35ac645001cadc6c1cffb2fbe6c7ba1
Andrew Gillis <gillis.andrewj@gmail.com>
2018-06-01 20:05:02 -0400

    Update doc to describe waiting queue.

--- doc.go
+++ doc.go
@@ -11,13 +11,14 @@ Non-blocking task submission
 A task is a function submitted to the worker pool for execution.  Submitting
 tasks to this worker pool will not block, regardless of the number of tasks.
 Tasks read from the input task queue are immediately dispatched to an available
-worker.  If no worker is immediately available, then the task is passed to a go
-routine which is started to wait for an available worker.  This clears the task
-from the task queue immediately, whether or not a worker is currently
-available, and will not block the submission of tasks.
+worker.  If no worker is immediately available, or there are already tasks
+waiting for an available worker, then the task is put on a waiting queue to
+wait for an available worker.  This clears the incoming task from the task
+queue immediately, whether or not a worker is currently available, and will not
+block the submission of tasks.
 
 The intent of the worker pool is to limit the concurrency of task execution,
-not limit the number of tasks queued to be executed. Therefore, this unbounded
+not limit the number of tasks queued to be executed.  Therefore, this unbounded
 input of tasks is acceptable as the tasks cannot be discarded.  If the number
 of inbound tasks is too many to even queue for pending processing, then the
 solution is outside the scope of workerpool, and should be solved by
@@ -30,10 +31,9 @@ Dispatcher
 This worker pool uses a single dispatcher goroutine to read tasks from the
 input task queue and dispatch them to a worker goroutine.  This allows for a
 small input channel, and lets the dispatcher queue as many tasks as are
-submitted when there are no available workers (using goroutines).
-Additionally, the dispatcher can adjust the number of workers as appropriate
-for the work load, without having to utilize locked counters and checks
-incurred on task submission.
+submitted when there are no available workers.  Additionally, the dispatcher
+can adjust the number of workers as appropriate for the work load, without
+having to utilize locked counters and checks incurred on task submission.
 
 When no tasks have been submitted for a period of time, a worker is removed by
 the dispatcher.  This is done until there are no more workers to remove.  The
@@ -46,6 +46,14 @@ It is advisable to use different worker pools for tasks that are bound by
 different resources, or that have different resource use patterns.  For
 example, tasks that use X Mb of memory may need different concurrency limits
 than tasks that use Y Mb of memory.
+
+Waiting queue vs goroutines
+
+When there are no available workers to handle incoming tasks, the tasks are put
+on a waiting queue.  In previous versions of workerpool, these tasks were
+passed to goroutines.  Using a queue is faster and has less memory overhead
+than creating a separate goroutine for each waiting task, allowing a much
+higher number of waiting tasks.
 
 Credits
 
