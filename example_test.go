a1f89466ae86a32ffaf58871634e4f4697b2ed35
Andrew Gillis <gillis.andrewj@gmail.com>
2018-09-20 10:50:12 -0400

    example test

--- example_test.go
+++ example_test.go
@@ -2,6 +2,7 @@ package workerpool
 
 import (
 	"fmt"
+	"sync/atomic"
 	"testing"
 )
 
@@ -9,18 +10,18 @@ func TestExample(t *testing.T) {
 	wp := New(2)
 	requests := []string{"alpha", "beta", "gamma", "delta", "epsilon"}
 
-	reqCount := 0
+	var reqCount int32
 	for _, r := range requests {
 		r := r
 		wp.Submit(func() {
 			fmt.Println("Handling request:", r)
-			reqCount++
+			atomic.AddInt32(&reqCount, 1)
 		})
 	}
 
 	wp.StopWait()
 
-	if reqCount < len(requests) {
+	if int(atomic.LoadInt32(&reqCount)) < len(requests) {
 		t.Fatal("Did not handle all requests")
 	}
 }
